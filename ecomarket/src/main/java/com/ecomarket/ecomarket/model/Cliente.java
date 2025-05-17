d63edd36f10dc4428f64c678f171e279ad784050
AmaroLeal <am.leal@duocuc.cl>
2025-05-17 15:18:16 -0400

    Clase Cliente y ClienteReposiroty

---
+++ ecomarket/src/main/java/com/ecomarket/ecomarket/model/Cliente.java
@@ -0,0 +1,68 @@
+package com.ecomarket.ecomarket.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String direccion;
+    private LocalDateTime fechaCreacion;
+
+
+    public Cliente() {}
+
+    
+    public Cliente(String nombre, String email, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.fechaCreacion = LocalDateTime.now(); 
+    }
+
+  
+    public Long getId() 
+    { return id; }
+    
+    public void setId(Long id) 
+    { this.id = id; }
+
+    public String getNombre() 
+    { return nombre; }
+    
+    public void setNombre(String nombre) 
+    { this.nombre = nombre; }
+
+    public String getEmail() 
+    { return email; }
+
+    public void setEmail(String email) 
+    { this.email = email; }
+
+    public String getTelefono() 
+    { return telefono; }
+    
+    public void setTelefono(String telefono) 
+    { this.telefono = telefono; }
+
+    public String getDireccion() 
+    { return direccion; }
+
+    public void setDireccion(String direccion) 
+    { this.direccion = direccion; }
+
+    public LocalDateTime getFechaCreacion() 
+    { return fechaCreacion; }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) 
+    { this.fechaCreacion = fechaCreacion; }
+}
